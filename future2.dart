ea0d9f876be9ec75676757389b64c2d51ca0cd2c
anddinifitriani <86968697+anddinifitriani@users.noreply.github.com>
2022-10-28 10:15:49 +0700

    Add files via upload

--- future2.dart
+++ future2.dart
@@ -1,9 +1,16 @@
-Future<String> getOrder(){
+Future<String> getOrder(n, name){
   return Future.delayed(Duration(seconds: 3), () {
-    return 'Kopi Hitam';
+    int stok = 5;
+    if (n <= stok)
+      return "$n $name";
+    else
+      throw "Stok tidak cukup";
   });
 }
 main(){
-  getOrder().then((value) => print("Pesanan Anda : $value"));
-  print("Membuat Pesanan");
+  getOrder(7, "Kopi Hitam")
+    .then((value) => print("Pesanan Anda : $value"))
+    .catchError((error) => print("Maaf, $error"))
+    .whenComplete(() => print("Terima kasih"));
+  print('Membuat Pesanan');
 }
